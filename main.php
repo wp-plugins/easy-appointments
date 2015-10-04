92bdcd9589a82827d9acead1f89f05049c560a1c
loncar <loncar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-04 10:39:13 +0000

    New version 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/easy-appointments/trunk@1259195 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * Plugin Name: Easy Appointments
  * Plugin URI: http://nikolaloncar.com/easy-appointments-wordpress-plugin/
  * Description: Simple managment of Appointments
- * Version: 1.4.0
+ * Version: 1.4.1
  * Author: Nikola Loncar
  * Author URI: http://nikolaloncar.com
  * Text Domain: easy-appointments
